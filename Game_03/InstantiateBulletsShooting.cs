fa608b5af50817d5cc9c90e4fc430f281ea0d3e6
kosmoss2304 <120576211+kosmoss2304@users.noreply.github.com>
2023-11-19 18:31:34 +0300

    f

---
+++ Game_03/InstantiateBulletsShooting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+[RequireComponent(typeof(Transform))]
+
+public class InstantiateBulletsShooting : MonoBehaviour
+{
+    [SerializeField] private float _speedBullet;
+    [SerializeField] private GameObject _prefabBullet;
+    [SerializeField] private float _rateFire;
+    [SerializeField] private Transform _target;
+
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+        var shootByTargetJob = StartCoroutine(ShootByTarget());
+    }
+
+    private IEnumerator ShootByTarget()
+    {
+        bool isShooting = true;
+
+        while (isShooting)
+        {
+            Vector3 direction = (_target.position - transform.position).normalized;
+
+            GameObject bullet = Instantiate(_prefabBullet, _transform.position, Quaternion.identity);
+
+            bullet.transform.up = direction;
+            bullet.GetComponent<Rigidbody2D>().AddForce(direction * _speedBullet, ForceMode2D.Impulse);
+
+            yield return new WaitForSecondsRealtime(_rateFire);
+        }
+    }
+}
